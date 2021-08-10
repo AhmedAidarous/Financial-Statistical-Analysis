4e21d4a3e5ea74f2f57e23aec2a1b9d19ec341b9
Ahmed AlAidarous <47617364+AhmedAidarous@users.noreply.github.com>
2021-08-10 22:06:49 +0100

    commit

--- indicators/BollingerBands.py
+++ indicators/BollingerBands.py
@@ -1,15 +1,29 @@
-def bollingerBands(stockData , n):
+
+def bollingerBands(stockData, n):
+    def plotlyPlot(title, stock):
+        """Method: Displays an interactive representation of given stock data in a
+           line graph on your browser"""
+        fig = px.line(title=title)
+
+        fig.add_scatter(x=stock['Date'], y=stock["BB_up"], name="BB_up")
+        fig.add_scatter(x=stock['Date'], y=stock["MA"], name="MA", fill="tonexty")
+        fig.add_scatter(x=stock['Date'], y=stock["BB_dn"], name="BB_dn", fill="tonexty")
+
+        fig.show()
+
+
+
     """
     The standard deviation measures how spread out numbers are form an average value.
     The width the band displays the volatility, the wider the bands, the larger the volatility.
     """
-    
+
     dataFrame = stockData.copy()
     dataFrame["MA"] = dataFrame["Adj Close"].rolling(n).mean()
-    dataFrame["BB_up"] = dataFrame["MA"] + 2*dataFrame["MA"].rolling(n).std()
-    dataFrame["BB_dn"] = dataFrame["MA"] - 2*dataFrame["MA"].rolling(n).std()
+    dataFrame["BB_up"] = dataFrame["MA"] + 2 * dataFrame["MA"].rolling(n).std()
+    dataFrame["BB_dn"] = dataFrame["MA"] - 2 * dataFrame["MA"].rolling(n).std()
     dataFrame["BB_range"] = dataFrame["BB_up"] - dataFrame["BB_dn"]
     dataFrame.dropna(inplace=True)
-    dataFrame[["MA" , "BB_up" , "BB_dn"]].plot()
-    plt.show()
+
+    plotlyPlot("BolingerBands" ,dataFrame[["Date","MA", "BB_up", "BB_dn"]] )
     return dataFrame
