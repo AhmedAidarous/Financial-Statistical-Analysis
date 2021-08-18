38114003413f8c972cf922d094ab1e4e05969ce3
Ahmed AlAidarous <47617364+AhmedAidarous@users.noreply.github.com>
2021-08-18 05:39:39 +0100

    Update CAGR.py

--- PerformanceMeasures/CAGR/CAGR.py
+++ PerformanceMeasures/CAGR/CAGR.py
@@ -1,12 +1,22 @@
+import pandas_datareader.data as pdr
+import numpy as np
+import datetime
+
+# Retrieve historic data from required stocks..
+
+ticker = "AAPL"
+
+# Ensure the data is daily
+Snp = pdr.get_data_yahoo(ticker , datetime.date.today() - datetime.timedelta(1825) , datetime.date.today())
 
 
+def CAGR(dataFrame):
+    dataFrame["DailyReturn"] = dataFrame["Adj Close"].pct_change()
+    dataFrame["CumulativeReturn"] = (1 + dataFrame["DailyReturn"]).cumprod()
 
+    # Getting the number of years given daily data
+    n = len(dataFrame) / 252
+    CAGR = (dataFrame["CumulativeReturn"][-1]) ** (1/n) - 1
 
-
-
-
-
-
-
-
+    return CAGR
 
