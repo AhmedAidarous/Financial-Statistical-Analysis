8697b5f1c2c5a26ff4550d84f5b5ade7e1265b5a
Ahmed AlAidarous <47617364+AhmedAidarous@users.noreply.github.com>
2021-08-23 12:31:57 +0100

    Update EWMA.py

--- StatisticalModels/EWMA.py
+++ StatisticalModels/EWMA.py
@@ -1,1 +1,19 @@
+# Importing modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+airline = pd.read_csv('airline_passengers.csv',index_col="Month")
+
+# Drop all NAN values
+airline.dropna(inplace=True)
+# Establishing the index to be the dates..
+airline.index = pd.to_datetime(airline.index)
+
+# Creating an exponential simple moving average
+airline['EWMA12'] = airline['Thousands of Passengers'].ewm(span=12).mean()
+airline['Thousands of Passengers'].plot(color="red")
+airline['EWMA12'].plot(color="blue")
+plt.legend()
+
+plt.show()
